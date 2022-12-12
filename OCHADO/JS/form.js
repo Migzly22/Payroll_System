eb472ac86b7c0f82dd96824880ddab37f9398ff2
Migzly22 <110314565+Migzly22@users.noreply.github.com>
2022-12-12 09:00:07 +0800

    Add files via upload

---
+++ OCHADO/JS/form.js
@@ -0,0 +1,32 @@
+var lname = document.getElementById('lname')
+var fname = document.getElementById('fname')
+
+fname.addEventListener("input",()=>{
+    fname.value = fname.value.toUpperCase();
+})
+lname.addEventListener("input",()=>{
+    lname.value = lname.value.toUpperCase();
+})
+
+
+function requiredpattern(event){
+    return /[A-Za-z ]/i.test(event.key)
+}
+
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
+
+sign_up_btn.addEventListener("click", () => {
+  container.classList.add("sign-up-mode");
+});
+
+sign_in_btn.addEventListener("click", () => {
+  container.classList.remove("sign-up-mode");
+});
+
+
+function ex(e){
+    e.parentElement.style.display="none";
+}
+
