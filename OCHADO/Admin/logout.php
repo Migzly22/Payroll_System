c250ec20be88fac781bb86bcaf18dc9216dad0f8
Migzly22 <110314565+Migzly22@users.noreply.github.com>
2022-12-23 18:57:55 +0800

    END OF PREVERSION

--- OCHADO/Admin/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-ob_start();
-
-unset($_SESSION["Login_Credits"]);
-header("Location:../Form2/index.php");
-ob_end_flush();
-exit;
--- OCHADO/DATABASE/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "ochado_db";// waiting
-
-$con = mysqli_connect($servername,$username,$password,$dbname);
